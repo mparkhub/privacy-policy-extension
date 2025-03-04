3778fecf9833707b70e404c9a27ca14b25dd960e
StahlSol <imperialistix@gmail.com>
2025-03-03 18:24:50 -0700

    Updated content.js to search for privacy policies

--- content.js
+++ content.js
@@ -1,1 +1,20 @@
 console.log("content.js script is running!");
+
+
+function findPrivacyLinks() {
+    let privacyLinks = [...document.querySelectorAll('a')].filter(a => a.innerText.toLowerCase().includes('privacy'));
+
+    if (privacyLinks.length > 0) {
+        console.log("Privacy policy found: ", privacyLinks[0].href);
+    } else {
+        console.log("No privacy policy found.");
+    }
+}
+
+findPrivacyLinks();
+
+const observer = new MutationObserver(() => {
+    findPrivacyLinks();
+});
+
+observer.observe(document.body, {childList: true, subtree: true});
