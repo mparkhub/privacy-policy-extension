3233c38771db391d5bcaf4898201c54e174034d1
Morgan Parker <mparker2@uccs.edu>
2025-04-04 11:56:59 -0600

    content now searches footers and sidebars for privacy policy links

--- content.js
+++ content.js
@@ -1,20 +1,44 @@
 console.log("content.js script is running!");
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function findPrivacyLinks() {
-    let privacyLinks = [...document.querySelectorAll('a')].filter(a => a.innerText.toLowerCase().includes('privacy'));
+    let footerLinks = [...document.querySelectorAll('footer a')]
+        .filter(a => a.innerText.toLowerCase().includes('privacy'));
+
+    if (footerLinks.length > 0) {
+        console.log("Privacy policy found in footer: ", footerLinks[0].href);
+        return footerLinks[0].href;
+    }
+
+    const menuButton = document.querySelector('button[aria-label="More Options"]');
+    if(menuButton) {
+        menuButton.click();
+        sleep(1000).then(() => {
+            let sideBarLinks = [...document.querySelectorAll('a')]
+                .filter(a => a.innerText.toLowerCase().includes('privacy'));
+                
+            if(sideBarLinks.length > 0) {
+                console.log("Privacy policy found in sidebar: ", sideBarLinks[0].href);
+                return sideBarLinks[0].href;
+            } else {
+                console.log("No privacy policy found in sidebar. Searching the rest of the page.");
+            }
+        });
+    } else {
+        console.log("No menu button found. Searching the rest of the page.");
+    }
+
+    let privacyLinks = [...document.querySelectorAll('a')]
+        .filter(a => a.innerText.toLowerCase().includes('privacy'));
 
     if (privacyLinks.length > 0) {
         console.log("Privacy policy found: ", privacyLinks[0].href);
     } else {
-        console.log("No privacy policy found.");
+        console.log("No privacy policy found.\nAttempting common URLs.");
     }
 }
 
 findPrivacyLinks();
-
-const observer = new MutationObserver(() => {
-    findPrivacyLinks();
-});
-
-observer.observe(document.body, {childList: true, subtree: true});
